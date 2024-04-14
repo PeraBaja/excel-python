3adfa7db2f1a895ea07c15a73195f3de0fa30aa7
Benjamin Peralta <benjaminp158@gmail.com>
2024-04-14 09:55:43 -0300

    Creada la clase agrupaciones

--- agrupacion.py
+++ agrupacion.py
@@ -1,13 +1,28 @@
 import numpy as np
 from Intervalo import Intervalo 
+class Agrupaciones:
+    def __init__(self, datos, cantidadDeAgrupaciones, redondearAbajo) -> None:
+        self.__datos = datos
+        self.__redondearAbajo = redondearAbajo
+        self.__cantidadDeAgrupaciones = cantidadDeAgrupaciones
+        anchoClase = self.__calcularAnchoClase(min(datos), max(datos))
+        self.__agrupaciones = self.__agrupar(datos)
+    
+    
+    def __agrupar(datos: list, anchoClase: float) -> tuple:
+        intervalos = []
+        for i in np.arange(min(datos), max(datos), anchoClase):
+            intervalos.append(Intervalo(i, i + anchoClase))
+        return tuple(intervalos)
 
-def agrupar(datos: list, anchoClase: float) -> tuple:
-    intervalos = []
-    for i in np.arange(min(datos), max(datos), anchoClase):
-        intervalos.append(Intervalo(i, i + anchoClase, datos))
-    return intervalos
+    def __calcularAnchoClase (self, valorMinimo, valorMaximo):
+        ancho_clase: float = (valorMaximo - valorMinimo) / self.__cantidadDeAgrupaciones
+        return ancho_clase.__floor__()  if self.__redondearAbajo else ancho_clase.__ceil__()
 
-def calcularAnchoClase (valorMinimo, valorMaximo, numeroDeAgrupaciones, redondearAbajo):
-    ancho_clase: float = (valorMaximo - valorMinimo) / numeroDeAgrupaciones
-    return ancho_clase.__floor__()  if redondearAbajo else ancho_clase.__ceil__()
+    def frecuenciasAbsolutas(self) :
+        for intervalo in self.__agrupaciones:
+            frecuenciaAbsoluta = sum(1 for dato in self.__datos if intervalo.limiteInferior <= dato < intervalo.limiteSuperior)
+            return frecuenciaAbsoluta
 
+    def frecuenciaRelativa(self):
+        return round(self.frecuenciaAbsoluta() / len(self.__datos), 2)
