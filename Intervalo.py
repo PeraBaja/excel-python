b01f978a4f68ff8d736e15dbc8067d63457142eb
Benjamin Peralta <benjaminp158@gmail.com>
2024-04-14 09:57:37 -0300

    extraida la logica de frecuencia absoluta y frecuencia relativa a agrupaciones

--- Intervalo.py
+++ Intervalo.py
@@ -1,15 +1,7 @@
 class Intervalo:
-    def __init__(self, limiteInferior, limiteSuperior, datos) :
+    def __init__(self, limiteInferior, limiteSuperior) :
         self.__limiteInferior = limiteInferior
         self.__limiteSuperior = limiteSuperior
-        self.__datos = datos
-
-    def frecuenciaAbsoluta(self) :
-        frecuenciaAbsoluta = sum(1 for dato in self.__datos if self.__limiteInferior <= dato < self.__limiteSuperior)
-        return frecuenciaAbsoluta
-
-    def frecuenciaRelativa(self):
-        return round(self.frecuenciaAbsoluta() / len(self.__datos), 2)
 
     def __str__(self):
         return f'{self.__limiteInferior}-{self.__limiteSuperior}'
