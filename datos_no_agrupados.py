8b379aa8bb5bdc433a9f8e121b0d2bc8e96f0f04
Benjamin Peralta <benjaminp158@gmail.com>
2024-04-14 21:37:00 -0300

    Corregido error en la comprobacion de la mediana y agregado logica funcion moda para datos no agrupados

--- datos_no_agrupados.py
+++ datos_no_agrupados.py
@@ -1,11 +1,28 @@
 def media (datos: list):
     return round(sum(datos) / len(datos), 2)
+
 def mediana (datos):
     posicionMedio = (len(datos) + 1) / 2
-    if type(posicionMedio) == int:
-        return posicionMedio
-    
+    if posicionMedio.is_integer():
+        return datos[int(posicionMedio)]    
     posicion1, posicion2 = (posicionMedio.__floor__(), posicionMedio.__ceil__())
-    return (datos[posicion2] - datos[posicion1]) / 2 
+    return (datos[posicion2] + datos[posicion1]) / 2 
+
 def moda (datos):
-    return max(datos)
+
+    aparicionMasGrande = 0
+    cantidadApariciones = __listar_apariciones(datos)
+    for dato in cantidadApariciones:
+        if cantidadApariciones[dato] > aparicionMasGrande:
+            aparicionMasGrande = cantidadApariciones[dato]
+            moda = dato
+    return moda
+
+def __listar_apariciones(datos) -> dict:
+    cantidadApariciones: dict = {}
+    for dato in datos:
+        if not cantidadApariciones.__contains__(dato):
+            cantidadApariciones.update({dato: 1})
+        else:
+            cantidadApariciones[dato] += 1
+    return cantidadApariciones
