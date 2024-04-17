46dde4f85e5a1138ff7214942de77e5a0323a0d6
Benjamin Peralta <benjaminp158@gmail.com>
2024-04-17 15:43:29 -0300

    Refactorizado la clase intervalos para funcionar como una dataclass

--- Intervalo.py
+++ Intervalo.py
@@ -1,21 +1,9 @@
-class Intervalo:
-    def __init__(self, limiteInferior, limiteSuperior) :
-        self.__limiteInferior = limiteInferior
-        self.__limiteSuperior = limiteSuperior
-    
-    @property
-    def limiteInferior(self):
-      return self.__limiteInferior
-    
-    @property
-    def limiteSuperior(self):
-      return self.__limiteSuperior
+from dataclasses import dataclass, field
 
-    @property
-    def marcaClase(self):
-        return  (self.__limiteSuperior + self.__limiteInferior) / 2
-    
-    def __str__(self):
-        return f'{self.__limiteInferior}-{self.__limiteSuperior}'
+@dataclass(frozen=True, slots=True)
+class Intervalo:
+    limiteInferior: float
+    limiteSuperior: float
+    marcaClase = field(init=False, default=(limiteSuperior + limiteInferior) / 2)
 
     
