e9ead7ab0819d2b717af06a77767aee8f0630974
Benjamin Peralta <benjaminp158@gmail.com>
2024-04-17 15:26:12 -0300

    Agregado funcion calcular percentil y calcular cuartiles en datos no agrupados

--- datos_no_agrupados.py
+++ datos_no_agrupados.py
@@ -1,15 +1,16 @@
-def media (datos: list):
+def media(datos: list):
     return round(sum(datos) / len(datos), 2)
 
-def mediana (datos):
+
+def mediana(datos):
     posicionMedio = (len(datos) + 1) / 2
     if posicionMedio.is_integer():
-        return datos[int(posicionMedio)]    
+        return datos[int(posicionMedio)]
     posicion1, posicion2 = (posicionMedio.__floor__(), posicionMedio.__ceil__())
-    return (datos[posicion2] + datos[posicion1]) / 2 
+    return (datos[posicion2] + datos[posicion1]) / 2
 
-def moda (datos):
 
+def moda(datos: list):
     aparicionMasGrande = 0
     cantidadApariciones = __listar_apariciones(datos)
     for dato in cantidadApariciones:
@@ -18,11 +19,36 @@ def moda (datos):
             moda = dato
     return moda
 
-def __listar_apariciones(datos) -> dict:
+
+def __listar_apariciones(datos: list) -> dict:
     cantidadApariciones: dict = {}
+    
     for dato in datos:
         if not cantidadApariciones.__contains__(dato):
             cantidadApariciones.update({dato: 1})
         else:
             cantidadApariciones[dato] += 1
+            
     return cantidadApariciones
+
+
+def calcular_cuartiles(datos: list) -> dict:
+    cuartiles = {}
+    cuartil = {}
+    for p in range(25, 100, 25):
+        cuartil = calcular_percentil(datos, p)
+        cuartiles.update(cuartil)
+    return cuartiles
+        
+def calcular_percentil(datos: list, percentilDeseado: int):
+    datos.sort()
+    
+    posicionPercentil = (percentilDeseado / 100) * len(datos)
+        
+    if posicionPercentil.is_integer():
+        posicionPercentil = int(posicionPercentil)
+        percentil = (datos[posicionPercentil - 1] + datos[posicionPercentil]) / 2
+    else:
+        posicionPercentil = posicionPercentil.__ceil__()
+        percentil = datos[posicionPercentil - 1]
+    return {posicionPercentil: percentil}
