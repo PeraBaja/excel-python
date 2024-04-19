037b169231e08852a8b50d0e926746e9031198fb
Benjamin Peralta <benjaminp158@gmail.com>
2024-04-19 12:02:07 -0300

    Optimizado llamado a la propiedad frecuencias absolutas. Ahora solo se calculan una vez

--- agrupacion.py
+++ agrupacion.py
@@ -7,8 +7,10 @@ class Agrupaciones:
         self.__redondearAbajo = redondearAbajo
         self.__cantidadDeAgrupaciones = cantidadDeAgrupaciones
         self.__anchoClase = self.anchoClase
-        self.__agrupaciones = self.__agrupar()
+        self.__agrupaciones = self.__agrupar() 
+        self.__frecuenciasAbsolutas = self.__calcular_frecuenciasAbsolutas()
         
+    frecuenciasAbsolutas = property(lambda self: self.__frecuenciasAbsolutas)
     def __agrupar(self) -> tuple:
         intervalos = []
         for i in np.arange(min(self.__datos), max(self.__datos), self.__anchoClase):
@@ -19,9 +21,8 @@ class Agrupaciones:
     def anchoClase (self):
         ancho_clase: float = (max(self.__datos) - min(self.__datos)) / self.__cantidadDeAgrupaciones
         return ancho_clase.__floor__()  if self.__redondearAbajo else ancho_clase.__ceil__()
-
-    @property
-    def frecuenciasAbsolutas(self) :
+    
+    def __calcular_frecuenciasAbsolutas(self) :
         frecuenciasAbsolutas = []
         for intervalo in self.__agrupaciones:
             intervalo: Intervalo
