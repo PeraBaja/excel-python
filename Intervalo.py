a33b4c28ad7db01184a8a96fda47b41d1a14e2ae
Benjamin Peralta <benjaminp158@gmail.com>
2024-04-19 12:36:07 -0300

    Arreglado error en calculo de la marca de clase

--- Intervalo.py
+++ Intervalo.py
@@ -4,6 +4,10 @@ from dataclasses import dataclass, field
 class Intervalo:
     limiteInferior: float
     limiteSuperior: float
-    marcaClase = field(init=False, default=(limiteSuperior + limiteInferior) / 2)
-
+    marcaClase: float = field(init=False)
     
+    def __post_init__(self):
+          object.__setattr__(self, 'marcaClase', (self.limiteInferior + self.limiteSuperior) / 2)
+    
+    def __repr__(self) -> str:
+         return f'{self.limiteInferior}-{self.limiteSuperior}'
