7c0e1243674f31ae5e8358b34ade78567247744c
Benjamin Peralta <benjaminp158@gmail.com>
2024-04-20 11:18:41 -0300

    Creada interfaz IMedible

---
+++ IMedible.py
@@ -0,0 +1,21 @@
+from abc import abstractmethod
+class IMedible:
+    @abstractmethod
+    def media():
+        pass
+
+    @abstractmethod
+    def mediana():
+        pass
+    
+    @abstractmethod
+    def moda():
+        pass
+    
+    @abstractmethod
+    def calcular_percentil():
+        pass
+    
+    @abstractmethod
+    def calcular_cuartiles():
+        pass
