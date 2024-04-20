ec5131459dcc6b998cf8c287b3abff9c484f8aac
Benjamin Peralta <benjaminp158@gmail.com>
2024-04-20 10:11:29 -0300

    Mejorado el rendimiento haciendo una vez el calculo de las frecuencias y el ancho de clase

--- agrupacion.py
+++ agrupacion.py
@@ -6,19 +6,23 @@ class Agrupaciones:
         self.__datos: tuple = datos
         self.__redondearAbajo = redondearAbajo
         self.__cantidadDeAgrupaciones = cantidadDeAgrupaciones
-        self.__anchoClase = self.anchoClase
+        self.__anchoClase = self.__calcular_anchoClase()
         self.__agrupaciones = self.__agrupar() 
         self.__frecuenciasAbsolutas = self.__calcular_frecuenciasAbsolutas()
-        
+        self.__frecuenciasRelativas = self.__calcular_frecuenciasRelativas()
+        self.__frecuenciasAcumuladas = self.__calcular_frecuenciasAcumuladas()
     frecuenciasAbsolutas = property(lambda self: self.__frecuenciasAbsolutas)
+    anchoClase = property(lambda self: self.__anchoClase)
+    frecuenciasRelativas = property(lambda self: self.__frecuenciasRelativas)
+    frecuenciasAcumuladas = property(lambda self: self.__frecuenciasAcumuladas)
+    
     def __agrupar(self) -> tuple:
         intervalos = []
         for i in np.arange(min(self.__datos), max(self.__datos), self.__anchoClase):
             intervalos.append(Intervalo(i, i + self.__anchoClase))
         return tuple(intervalos)
     
-    @property
-    def anchoClase (self):
+    def __calcular_anchoClase (self):
         ancho_clase: float = (max(self.__datos) - min(self.__datos)) / self.__cantidadDeAgrupaciones
         return ancho_clase.__floor__()  if self.__redondearAbajo else ancho_clase.__ceil__()
     
@@ -29,18 +33,17 @@ class Agrupaciones:
             frecuenciasAbsolutas.append(
                 sum(1 for dato in self.__datos if intervalo.limiteInferior <= dato < intervalo.limiteSuperior)
                 )
-        return tuple(frecuenciasAbsolutas)    
+        return tuple(frecuenciasAbsolutas)      
 
-    @property
-    def frecuenciasRelativas(self):
+    def __calcular_frecuenciasRelativas(self):
         frecuenciasRelativas = []
         for frecuencia in self.frecuenciasAbsolutas: 
             frecuenciasRelativas.append(
                 round(frecuencia / len(self.__datos), 2)
                 )
         return tuple(frecuenciasRelativas)
-    @property
-    def frecuenciasAcumuladas(self):
+
+    def __calcular_frecuenciasAcumuladas(self):
         frecuenciaAcumulada = 0
         frecuenciasAcumuladas = []
         for frecuenciaAbsoluta in self.frecuenciasAbsolutas:
