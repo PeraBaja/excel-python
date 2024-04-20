9124164dc92d934ae85e4bd2fc42a9e2434a4e37
Benjamin Peralta <benjaminp158@gmail.com>
2024-04-20 10:12:16 -0300

    Modificado el main para mostrar la nueva información

---
+++ Frecuencia.py
@@ -0,0 +1,7 @@
+from enum import Enum, auto
+
+class TipoFrecuencia(Enum):
+    ABSOLUTA = auto()
+    RELATIVA = auto()
+    ACUMULADA = auto()
+    PORCENTUAL = auto()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from agrupacion import Agrupaciones
-from datos_no_agrupados import media, mediana, moda 
+from datos_no_agrupados import calcular_percentil, media, mediana, moda, calcular_cuartiles
 from Intervalo import Intervalo
+
 kmRecorridosCon1LDeNafta = [
   12.5, 11.3, 13.25, 9.5, 10.5,
   13.25, 9.75, 9.25, 7.5, 9.5,
@@ -10,8 +11,13 @@ kmRecorridosCon1LDeNafta = [
   13, 9.25, 9.5, 10.9, 11.5,
   9.75, 13, 8.75, 13.25, 9.25,
   7.8, 12.75, 10.25, 12.75, 7.8,
-  8.75, 11.75, 11.5, 11.5, 9.4, 8
+  8.75, 11.75, 11.5, 11.5, 9.4
 ]
 agrupaciones = Agrupaciones(kmRecorridosCon1LDeNafta, 7, False)
-print(agrupaciones, agrupaciones.anchoClase, agrupaciones.frecuenciasAbsolutas, agrupaciones.frecuenciasRelativas)
+print(agrupaciones)
+print(f'Media: {agrupaciones.media()}, Mediana: {agrupaciones.mediana()}, Moda: {agrupaciones.moda()}')
 print(f'Media: {media(kmRecorridosCon1LDeNafta)}, Mediana: {mediana(kmRecorridosCon1LDeNafta)}, Moda: {moda(kmRecorridosCon1LDeNafta)}')
+print(f'Cuartiles: {calcular_cuartiles(kmRecorridosCon1LDeNafta)}')
+print(f'Percentil: {calcular_percentil(kmRecorridosCon1LDeNafta, 13)}')
+
+print(sorted(kmRecorridosCon1LDeNafta))
