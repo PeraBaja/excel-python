29da422757938fcf31392cee52510001e16cf34e
Benjamin Peralta <benjaminp158@gmail.com>
2024-04-24 13:55:09 -0300

    Corregido error no era suscribible a __agrupacion

--- MedicionesAgrupacion.py
+++ MedicionesAgrupacion.py
@@ -4,11 +4,12 @@ from Frecuencias import Frecuencias
 
 class MedicionesAgrupacion:
     def __init__(self, agrupacion: Agrupacion, frecuencias: Frecuencias, datos: tuple):
+        self.__intervalos = agrupacion.intervalos
         self.__agrupacion = agrupacion
         self.__frecuencias = frecuencias
         self.__datos = datos
     def media(self):
-        resultado = [intervalo.marcaClase * Fi for intervalo, Fi in zip(self.__agrupacion, self.__frecuencias.absolutas)]
+        resultado = [intervalo.marcaClase * Fi for intervalo, Fi in zip(self.__intervalos, self.__frecuencias.absolutas)]
         return round(sum(resultado) / len(self.__datos), 2)
 
     def mediana(self):
@@ -23,7 +24,7 @@ class MedicionesAgrupacion:
                 except IndexError:
                     FiAnterior = 0
                 fi = self.__frecuencias.absolutas[i]
-                Li = self.__agrupacion[i].limiteInferior
+                Li = self.__intervalos[i].limiteInferior
                 break #Si encontramos el inmediato mayor a n/2 entonces dejamos de buscar
         return round(Li + ((Me - FiAnterior) / fi)* self.__agrupacion.anchoClase, 2)
     
@@ -37,7 +38,7 @@ class MedicionesAgrupacion:
             if self.__frecuencias.absolutas[i] > Mo:
                 Mo = self.__frecuencias.absolutas[i]
                 fi = self.__frecuencias.absolutas[i]
-                Li = self.__agrupacion[i].limiteInferior
+                Li = self.__intervalos[i].limiteInferior
                 try:
                     fiAnterior = self.__frecuencias.absolutas[i - 1]
                 except IndexError:
