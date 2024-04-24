bf89fb0eeb44a3ab9118fa1d2f5d687e64905de8
Benjamin Peralta <benjaminp158@gmail.com>
2024-04-24 12:32:19 -0300

    Removido lógica de calculo de la media mediana y moda

--- agrupacion.py
+++ agrupacion.py
@@ -19,46 +19,6 @@ class Agrupacion:
         ancho_clase: float = (max(self.__datos) - min(self.__datos)) / self.__cantidadDeIntervalos
         return ancho_clase.__floor__()  if self.__redondearAbajo else ancho_clase.__ceil__()
             
-    def media(self):
-        resultado = [intervalo.marcaClase * Fi for intervalo, Fi in zip(self.__agrupaciones, self.frecuenciasAbsolutas)]
-        return round(sum(resultado) / len(self.__datos), 2)
-
-    def mediana(self):
-        Me = sum(self.frecuenciasAbsolutas) / 2
-        FiAnterior = 0
-        fi = 0
-        Li = 0
-        for i in range(len(self.frecuenciasAcumuladas)):
-            if self.frecuenciasAcumuladas[i] > Me:
-                try:
-                    FiAnterior = self.frecuenciasAcumuladas[i - 1]
-                except IndexError:
-                    FiAnterior = 0
-                fi = self.frecuenciasAbsolutas[i]
-                Li = self.__intervalos[i].limiteInferior
-                break
-        return round(Li + ((Me - FiAnterior) / fi)* self.anchoClase, 2)
-    
-    def moda(self):
-        Mo = 0
-        fi = 0
-        fiAnterior = 0
-        fiPosterior = 0
-        Li = 0
-        for i in range(len(self.frecuenciasAbsolutas)):
-            if self.frecuenciasAbsolutas[i] > Mo:
-                Mo = self.frecuenciasAbsolutas[i]
-                fi = self.frecuenciasAbsolutas[i]
-                Li = self.__intervalos[i].limiteInferior
-                try:
-                    fiAnterior = self.frecuenciasAbsolutas[i - 1]
-                except IndexError:
-                    fiAnterior = 0
-                try:
-                    fiPosterior = self.frecuenciasAbsolutas[i + 1]
-                except IndexError:
-                    fiPosterior = 0
-        return round(Li + ((Mo - fiAnterior) / (fi - fiAnterior + fi - fiPosterior))* self.anchoClase, 2)
     
     def __iter__(self):
         return self
