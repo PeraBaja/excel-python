8c98643333ec6c706987211ee7d97280c175bb1f
Benjamin Peralta <benjaminp158@gmail.com>
2024-04-25 14:14:48 -0300

    Agregado el __init__ con sus respectivos imports

---
+++ excel-python/__init__.py
@@ -0,0 +1,5 @@
+from Agrupacion import Agrupacion
+from Frecuencias import Frecuencias
+from Intervalo import Intervalo
+from MedicionesAgrupacion import MedicionesAgrupacion
+from Muestra import Muestra
