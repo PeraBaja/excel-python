948b8861f0809297da88894201ec35e4928e1f4f
Benjamin Peralta <benjaminp158@gmail.com>
2024-04-25 15:19:30 -0300

    Documentado la clase Muestra y sus métodos

--- excel_python/Muestra.py
+++ excel_python/Muestra.py
@@ -2,14 +2,29 @@ from IMedible import IMedible
 
 
 class Muestra(IMedible):
-    def __init__(self, datos: list):
+    def __init__(self, datos: tuple):
+        """_Clase que permite hacer mediciones estadísticas a partir de los datos no agrupados_
+
+        Args:
+            datos (tuple)
+        """
         self.__datos = datos
         self.__media = self.media()
-    def media(self):
+    def media(self) -> float:
+        """_Devuelve el promedio de los datos no agrupados_
+
+        Returns:
+            float
+        """
         return round(sum(self.__datos) / len(self.__datos), 2)
 
 
-    def mediana(self):
+    def mediana(self) -> float:
+        """_Devuelve el promedio de los datos no agrupados_
+
+        Returns:
+            float
+        """
         self.__datos.sort()
         posicionMedio = (len(self.__datos) + 1) / 2
         if posicionMedio.is_integer():
@@ -18,7 +33,12 @@ class Muestra(IMedible):
         return (self.__datos[posicion2] + self.__datos[posicion1]) / 2
 
 
-    def moda(self):
+    def moda(self) -> float:
+        """_Devuelve el promedio de los datos no agrupados_
+
+        Returns:
+            float
+        """
         aparicionMasGrande = 0
         cantidadApariciones = self.__listar_apariciones()
         for dato in cantidadApariciones:
@@ -39,7 +59,16 @@ class Muestra(IMedible):
                 
         return cantidadApariciones
             
-    def calcular_percentil(self, percentilDeseado: int):
+    def calcular_percentil(self, percentilDeseado: int) -> dict:
+        """_Devuelve el percentil a partir de un percentil porcentual (p/100) 
+            de los datos no agrupados_
+
+        Args:
+            percentilDeseado (_float_): _El porcentaje del percentil_
+
+        Returns:
+            _dict_: _Retorna la posicion del percentil como clave y como valor el percentil_
+        """
         self.__datos.sort()
         
         posicionPercentil = (percentilDeseado / 100) * len(self.__datos)
@@ -51,7 +80,12 @@ class Muestra(IMedible):
             posicionPercentil = posicionPercentil.__ceil__()
             percentil = self.__datos[posicionPercentil - 1]
         return {posicionPercentil: percentil}
-    def varianza(self):
+    def varianza(self) -> float:
+        """_Devuelve la varianza de los datos no agrupados_
+
+        Returns:
+            float
+        """
         frecuenciasAbsolutas = self.__listar_apariciones() 
         sumatoria = sum(((dato - self.__media) ** 2) * frecuenciasAbsolutas[dato] for dato in set(self.__datos))
         return sumatoria / len(self.__datos) - 1
