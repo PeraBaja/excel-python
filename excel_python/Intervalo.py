ea8ed2cad7fb4a1e943ea61915e74608fc30b3de
Benjamin Peralta <benjaminp158@gmail.com>
2024-04-25 15:00:16 -0300

    Documentado la clase Intervalo

--- excel_python/Intervalo.py
+++ excel_python/Intervalo.py
@@ -2,6 +2,11 @@ from dataclasses import dataclass, field
 
 @dataclass(frozen=True, slots=True)
 class Intervalo:
+    """_Crea un intervalo con un limite inferior y un limite superior_
+    Args:
+        limiteInferior (float)
+        limiteSuperior (float)
+    """
     limiteInferior: float
     limiteSuperior: float
     marcaClase: float = field(init=False)
