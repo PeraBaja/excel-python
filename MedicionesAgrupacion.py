048a38f1a08908b30917fdb32e57e981149f1fc8
Benjamin Peralta <benjaminp158@gmail.com>
2024-04-25 13:18:34 -0300

    Agregadas las funciones calcular percentil, calcular cuartil y varianza

--- MedicionesAgrupacion.py
+++ MedicionesAgrupacion.py
@@ -1,8 +1,8 @@
 from Agrupacion import Agrupacion
 from Frecuencias import Frecuencias
+from IMedible import IMedible
 
-
-class MedicionesAgrupacion:
+class MedicionesAgrupacion(IMedible):
     def __init__(self, agrupacion: Agrupacion, frecuencias: Frecuencias, datos: tuple):
         self.__intervalos = agrupacion.intervalos
         self.__agrupacion = agrupacion
@@ -13,20 +13,7 @@ class MedicionesAgrupacion:
         return round(sum(resultado) / len(self.__datos), 2)
 
     def mediana(self):
-        Me = sum(self.__frecuencias.absolutas) / 2
-        FiAnterior = 0
-        fi = 0
-        Li = 0
-        for i in range(len(self.__frecuencias.acumuladas)):
-            if self.__frecuencias.acumuladas[i] > Me:
-                try:
-                    FiAnterior = self.__frecuencias.acumuladas[i - 1]
-                except IndexError:
-                    FiAnterior = 0
-                fi = self.__frecuencias.absolutas[i]
-                Li = self.__intervalos[i].limiteInferior
-                break #Si encontramos el inmediato mayor a n/2 entonces dejamos de buscar
-        return round(Li + ((Me - FiAnterior) / fi)* self.__agrupacion.anchoClase, 2)
+        return self.calcular_percentil(50)[50]
     
     def moda(self):
         Mo = 0
@@ -48,4 +35,35 @@ class MedicionesAgrupacion:
                 except IndexError:
                     fiPosterior = 0
         return round(Li + ((Mo - fiAnterior) / (fi - fiAnterior + fi - fiPosterior))* self.__agrupacion.anchoClase, 2)
-    
+        pass
+    def calcular_percentil(self, percentilDeseado: float):
+        """_summary_
+
+        Args:
+            percentilDeseado (_float_): _el porcentaje del percentil_
+
+        Returns:
+            _dict_: _retorna el percentil deseado_
+        """
+        Me = sum(self.__frecuencias.absolutas) * (percentilDeseado / 100)
+        FiAnterior = 0
+        fi = 0
+        Li = 0
+        for i in range(len(self.__frecuencias.acumuladas)):
+            if self.__frecuencias.acumuladas[i] > Me:
+                try:
+                    FiAnterior = self.__frecuencias.acumuladas[i - 1]
+                except IndexError:
+                    FiAnterior = 0
+                fi = self.__frecuencias.absolutas[i]
+                Li = self.__intervalos[i].limiteInferior
+                break #Si encontramos el inmediato mayor a n/2 entonces dejamos de buscar
+        pecentil = round(Li + ((Me - FiAnterior) / fi)* self.__agrupacion.anchoClase, 2)    
+        return {percentilDeseado: pecentil} 
+    def varianza(self):
+        sumatoria = 0
+        cantidadIntervalos = len(self.__intervalos)
+        for i in range(cantidadIntervalos):
+            sumatoria += (pow(self.__intervalos[i].marcaClase - self.media(), 2)) * self.__frecuencias.absolutas[i]
+        return sumatoria / len(self.__datos) - 1
+        
