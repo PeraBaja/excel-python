b5fcef2fccbbb362b1a1056de271ad0104eeacb1
Benjamin Peralta <benjaminp158@gmail.com>
2024-04-26 16:46:52 -0300

    Solucionando problemas de importacion de paquetes

--- estadistica_facil/__init__.py
+++ estadistica_facil/__init__.py
@@ -1,7 +1,9 @@
+"""_Permite realizar operaciones estadisticas de manera sencilla_"""
 from estadistica_facil.Agrupacion import Agrupacion
 from estadistica_facil.Frecuencias import Frecuencias
 from estadistica_facil.Intervalo import Intervalo
 from estadistica_facil.MedicionesAgrupacion import MedicionesAgrupacion
 from estadistica_facil.Muestra import Muestra
+__version__ = "1.0.17"
 print("Clases importadas correctamente")
 __all__ = ['Agrupacion', 'Frecuencias', 'Intervalo', 'MedicionesAgrupacion', 'Muestra']
