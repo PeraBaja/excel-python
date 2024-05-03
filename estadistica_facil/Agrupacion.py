e5a6f5d27804b208d6ef6e35a952a0501e3665e7
Benjamin Peralta <benjaminp158@gmail.com>
2024-05-03 12:01:04 -0300

    Validado error al tratar de usar un ancho de clase igual a 0

--- estadistica_facil/Agrupacion.py
+++ estadistica_facil/Agrupacion.py
@@ -12,10 +12,14 @@ def agrupar(datos: tuple, ancho_clase) -> tuple:
         redondearAbajo (bool): _Si al calcular el ancho de clase se redondea hacia abajo_
     """
     intervalos = []
-    for i in np.arange(min(datos), max(datos), ancho_clase):
-        intervalos.append(Intervalo(i, i + ancho_clase))
-    return tuple(intervalos)
-    
+    try:
+        for i in np.arange(min(datos), max(datos), ancho_clase):
+            intervalos.append(Intervalo(i, i + ancho_clase))
+    except ZeroDivisionError:
+        print("❌ Error al tratar de crear los intervalos. El ancho de clase debe ser mayor o igual a 1")
+    else:
+        return tuple(intervalos)
+
 def calcular_anchoClase(datos, cantidadDeIntervalos: int, redondearAbajo: bool):       
     ancho_clase: float = (max(datos) - min(datos)) / cantidadDeIntervalos
     return ancho_clase.__floor__()  if redondearAbajo else ancho_clase.__ceil__()
