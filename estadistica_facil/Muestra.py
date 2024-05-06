68bc1a7dcb03e5abe9967f246ed369fbecd2cd5f
Benjamin Peralta <benjaminp158@gmail.com>
2024-05-06 09:32:37 -0300

    Cambiado clase muestra por funciones

--- estadistica_facil/Muestra.py
+++ estadistica_facil/Muestra.py
@@ -1,92 +1,72 @@
 from estadistica_facil.IMedible import IMedible
 
 
-class Muestra(IMedible):
-    def __init__(self, datos: tuple):
-        """_Clase que permite hacer mediciones estadísticas a partir de los datos no agrupados_
 
-        Args:
-            datos (tuple)
-        """
-        self.__datos = datos
-        self.__media = self.media()
-    def media(self) -> float:
-        """_Devuelve el promedio de los datos no agrupados_
+def media(datos: list) -> float:
+    """_Devuelve el promedio de los datos no agrupados_
 
-        Returns:
-            float
-        """
-        return round(sum(self.__datos) / len(self.__datos), 2)
+    Returns:
+        float
+    """
+    return round(sum(datos) / len(datos), 2)
 
 
-    def mediana(self) -> float:
-        """_Devuelve el promedio de los datos no agrupados_
+def mediana(datos: tuple) -> float:
+    """_Devuelve el promedio de los datos no agrupados_
 
-        Returns:
-            float
-        """
-        self.__datos.sort()
-        posicionMedio = (len(self.__datos) + 1) / 2
-        if posicionMedio.is_integer():
-            return self.__datos[int(posicionMedio)]
-        posicion1, posicion2 = (posicionMedio.__floor__(), posicionMedio.__ceil__())
-        return (self.__datos[posicion2] + self.__datos[posicion1]) / 2
-
-
-    def moda(self) -> float:
-        """_Devuelve el promedio de los datos no agrupados_
-
-        Returns:
-            float
-        """
-        aparicionMasGrande = 0
-        cantidadApariciones = self.__listar_apariciones()
-        for dato in cantidadApariciones:
-            if cantidadApariciones[dato] > aparicionMasGrande:
-                aparicionMasGrande = cantidadApariciones[dato]
-                moda = dato
-        return moda
+    Returns:
+        float
+    """
+    datos = sorted(datos)
+    posicionMedio = (len(datos) + 1) / 2
+    if posicionMedio.is_integer():
+        return datos[int(posicionMedio)]
+    posicion1, posicion2 = (posicionMedio.__floor__(), posicionMedio.__ceil__())
+    return (datos[posicion2] + datos[posicion1]) / 2
 
 
-    def __listar_apariciones(self) -> dict:
-        cantidadApariciones: dict = {}
+def moda(datos: tuple, frecuencias_absolutas: tuple) -> float:
+    """_Devuelve el promedio de los datos no agrupados_
+
+    Returns:
+        float
+    """
+    frecuencia_maxima = max(frecuencias_absolutas)
+    i = frecuencias_absolutas.index(frecuencia_maxima)
+    return datos[i]
         
-        for dato in self.__datos:
-            if not cantidadApariciones.__contains__(dato):
-                cantidadApariciones.update({dato: 1})
-            else:
-                cantidadApariciones[dato] += 1
-                
-        return cantidadApariciones
-            
-    def calcular_percentil(self, percentilDeseado: int) -> dict:
-        """_Devuelve el percentil a partir de un percentil porcentual (p/100) 
-            de los datos no agrupados_
+def calcular_percentil(datos: tuple, percentilDeseado: int) -> dict:
+    """_Devuelve el percentil a partir de un percentil porcentual (p/100) 
+        de los datos no agrupados_
 
-        Args:
-            percentilDeseado (_float_): _El porcentaje del percentil_
+    Args:
+        percentilDeseado (_float_): _El porcentaje del percentil_
 
-        Returns:
-            _dict_: _Retorna la posicion del percentil como clave y como valor el percentil_
-        """
-        self.__datos.sort()
+    Returns:
+        _dict_: _Retorna la posicion del percentil como clave y como valor el percentil_
+    """
+    datos = sorted(datos)
+    
+    posicionPercentil = (percentilDeseado / 100) * len(datos)
         
-        posicionPercentil = (percentilDeseado / 100) * len(self.__datos)
-            
-        if posicionPercentil.is_integer():
-            posicionPercentil = int(posicionPercentil)
-            percentil = (self.__datos[posicionPercentil - 1] + self.__datos[posicionPercentil]) / 2
-        else:
-            posicionPercentil = posicionPercentil.__ceil__()
-            percentil = self.__datos[posicionPercentil - 1]
-        return {posicionPercentil: percentil}
-    def varianza(self) -> float:
-        """_Devuelve la varianza de los datos no agrupados_
-
-        Returns:
-            float
-        """
-        frecuenciasAbsolutas = self.__listar_apariciones() 
-        sumatoria = sum(((dato - self.__media) ** 2) * frecuenciasAbsolutas[dato] for dato in set(self.__datos))
-        return sumatoria / len(self.__datos) - 1
+    if posicionPercentil.is_integer():
+        posicionPercentil = int(posicionPercentil)
+        percentil = (datos[posicionPercentil - 1] + datos[posicionPercentil]) / 2
+    else:
+        posicionPercentil = posicionPercentil.__ceil__()
+        percentil = datos[posicionPercentil - 1]
         
+    return {posicionPercentil: percentil}
+
+def varianza(datos: tuple, frecuencias_absolutas: tuple) -> float:
+    """_Devuelve la varianza de los datos no agrupados_
+
+    Returns:
+        float
+    """
+    media_datos = media(datos)
+    sumatoria = 0
+    for dato, frecuencia in zip(datos, frecuencias_absolutas):
+        sumatoria += ((dato - media_datos) ** 2) * frecuencia
+    return sumatoria / len(datos) - 1 ##ERROR de calculo
+    
