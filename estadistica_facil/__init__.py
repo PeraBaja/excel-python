208e4a512975f753d8369b3ab0a499ebcd244e72
Benjamin Peralta <benjaminp158@gmail.com>
2024-05-06 11:17:44 -0300

    Actualizado importaciones

--- estadistica_facil/__init__.py
+++ estadistica_facil/__init__.py
@@ -1,9 +1,10 @@
 """_Permite realizar operaciones estadisticas de manera sencilla_"""
-from estadistica_facil.agrupacion import *
-from estadistica_facil.frecuencias import *
+from estadistica_facil.agrupacion import calcular_anchoClase, agrupar
+from estadistica_facil.frecuencias import frecuencias_absolutas, frecuencias_acumuladas, frecuencias_relativas
 from estadistica_facil.Intervalo import Intervalo
-from estadistica_facil.MedicionesAgrupacion import MedicionesAgrupacion
-from estadistica_facil.Muestra import Muestra
+import estadistica_facil.medicionesAgrupacion
+import estadistica_facil.muestra
+
 __version__ = "1.0.17"
 print("Clases importadas correctamente")
-__all__ = ['Agrupacion', 'Frecuencias', 'Intervalo', 'MedicionesAgrupacion', 'Muestra']
+__all__ = ['calcular_anchoClase', 'agrupar', 'frecuencias', 'Intervalo', 'medicionesAgrupacion', 'muestra']
