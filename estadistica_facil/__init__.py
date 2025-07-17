ca2ba3c59262e5cf5e746ca6cb1e2f5d8c38e8d4
Benjamín Peralta <benjaminp158@gmail.com>
2025-07-17 20:21:09 -0300

    Cambiado versión y recompilado los paquetes de la librería

--- estadistica_facil/__init__.py
+++ estadistica_facil/__init__.py
@@ -4,6 +4,6 @@ from estadistica_facil.Frecuencias import Frecuencias
 from estadistica_facil.Intervalo import Intervalo
 from estadistica_facil.MedicionesAgrupacion import MedicionesAgrupacion
 from estadistica_facil.Muestra import Muestra
-__version__ = "1.0.17"
+__version__ = "1.0.18"
 print("Clases importadas correctamente")
 __all__ = ['Agrupacion', 'Frecuencias', 'Intervalo', 'MedicionesAgrupacion', 'Muestra']
